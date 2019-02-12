7d91e02b1a14db060c4f841ac9c0fe9fa22fda4e
浅忆梦微凉 <3538922590@qq.com>
2019-02-12 21:44:49 +0800

    1.0测试通过

--- 剑指offer30_左旋转字符串/test.cpp
+++ 剑指offer30_左旋转字符串/test.cpp
@@ -9,15 +9,18 @@ public:
 		auto str_size = str.size();
 		string temp1 = str.substr(0, str_size - n);
 		string temp2 = str.substr(str_size - n, n);
-		str = temp1 + temp2;
+		str = temp2 + temp1;
 		return str;
 	}
 };
 
 void TestFunc(){
 	string test("hello world");
+	cout << "未旋转之前的字符串为：";
+	cout << test << " " << endl;
 	Solution s;
 	string test2 = s.LeftRotateString(test, 3);
+	cout << "旋转之后的字符串为：";
 	cout << test2 << " " << endl;
 }
 
