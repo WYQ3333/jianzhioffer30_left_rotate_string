4de41d63788eec4b8f83269b577c6f2df431b808
浅忆梦微凉 <3538922590@qq.com>
2019-02-12 21:42:43 +0800

    有点小问题

--- 剑指offer30_左旋转字符串/test.cpp
+++ 剑指offer30_左旋转字符串/test.cpp
@@ -10,11 +10,15 @@ public:
 		string temp1 = str.substr(0, str_size - n);
 		string temp2 = str.substr(str_size - n, n);
 		str = temp1 + temp2;
+		return str;
 	}
 };
 
 void TestFunc(){
-
+	string test("hello world");
+	Solution s;
+	string test2 = s.LeftRotateString(test, 3);
+	cout << test2 << " " << endl;
 }
 
 int main(){
